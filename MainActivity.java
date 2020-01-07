93b397930b14a630ca3cd46926547394265bc441
Bubinka <33425794+Bubinka@users.noreply.github.com>
2020-01-07 15:26:35 +0000

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,55 +1,139 @@
-package com.example.implicitinents;
+package com.community.rathcoolecommunitynotes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+   static ArrayList<String> notes = new ArrayList<>();
+   static ArrayAdapter arrayAdapter;
+
+   @Override
+
+   public boolean onCreateOptionsMenu(Menu menu) {
+
+       MenuInflater menuInflater = getMenuInflater();
+       menuInflater.inflate(R.menu.add_note_menu,menu);
+
+
+
+
+
+       return super.onCreateOptionsMenu(menu);
+   }
+
+   @Override
+
+   public  boolean onOptionsItemSelected(MenuItem item) {
+
+
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId()==R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
+   }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnwebaddress).setOnClickListener(this);
-        findViewById(R.id.btnmap).setOnClickListener(this);
-        findViewById(R.id.btnphonenumber).setOnClickListener(this);
-    }
+        ListView listView =(ListView) findViewById(R.id.listView);
 
-    @Override
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.community.rathcoolecommunitynotes", Context.MODE_PRIVATE);
 
-    public void onClick(View view){
+        HashSet<String>set =(HashSet<String>) sharedPreferences.getStringSet("notes",null);
 
-        Uri uri = null;
-        Intent intent = null;
+        if(set== null) {
 
-        switch(view.getId()) {
 
-            case R.id.btnwebaddress:
-                uri = Uri.parse("http://www.apress.com");
-                intent = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(intent);
-                break;
+            notes.add("Example note");
 
-            case R.id.btnmap:
-                uri = Uri.parse("geo:40.7113399-74.0263469");
-                intent = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(intent);
-                break;
-
-            case R.id.btnphonenumber:
-
-                uri = Uri.parse("tel: 639285083333");
-                intent = new Intent(Intent.ACTION_DIAL,uri);
-                startActivity(intent);
-                break;
-            default:
-
-                Log.i(getClass().getName(),"Cannot resolve button click");
+        }else {
+            notes = new ArrayList<>(set);
         }
+
+         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+                intent.putExtra("noteId",i);
+                startActivity(intent);
+
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                return false;
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                            @Override
+
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.community.rathcoolecommunitynotes", Context.MODE_PRIVATE);
+
+                                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes",set).apply();
+
+
+                            }
+                        })
+                        .setNegativeButton("No",null)
+                        .show();
+                return true;
+            }
+        });
     }
 }
