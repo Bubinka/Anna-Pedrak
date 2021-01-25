0c8fb5b0b0e2d882f89213e4bdfa3b54abea5d33
Bubinka <33425794+Bubinka@users.noreply.github.com>
2021-01-25 10:19:19 +0000

    Add files via upload

---
+++ MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.luasforecast;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Assert;
+
+public class MainActivityTest {
+
+    @Test
+    public void testInitialAlwaysPasses() {
+
+        assert true;
+    }//end method testInitialAlwaysPasses
+//end MainActivityTest class
+}
