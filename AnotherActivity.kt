6fa902d7b72171f9269fcf177ba288626317839c
Bubinka <33425794+Bubinka@users.noreply.github.com>
2021-08-05 10:39:22 +0100

    Add files via upload

--- AnotherActivity.kt
+++ AnotherActivity.kt
@@ -1,14 +1,33 @@
-package com.example.hrmeeting
+package com.example.sportdiscipline
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBar
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_another.*
 
 class AnotherActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_another)
 
-        val actionBar = supportActionBar
-        actionBar!!.title = "Goodbye"
+        val actionBar:ActionBar?= supportActionBar
+        actionBar !!.setDisplayHomeAsUpEnabled(true)
+        actionBar !!.setDisplayShowHomeEnabled(true)
+
+        //get data from putExtra
+
+        var intent = intent
+        val aTitle = intent.getStringExtra("iTitle")
+        val aDescription = intent.getStringExtra("iDescription")
+        val aImageView = intent.getIntExtra("ImageView",0)
+
+        //set title in another activity
+
+        actionBar.setTitle(aTitle)
+        a_title.text = aTitle
+        a_description.text = aDescription
+
+        imageView.setImageResource(aImageView)
     }
-}
+}//end class AnotherActivity
